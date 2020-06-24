d9767e6da9ec4873b7a88aa67650119e45720137
ParkDaeGwang <iieorhkd@naver.com>
2020-06-24 15:47:41 +0900

    어드민 폴더 구성

---
+++ teaspoon/src/main/java/com/kh/teaspoon/admin/controller/AdminCotroller.java
@@ -0,0 +1,8 @@
+package com.kh.teaspoon.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminCotroller {
+
+}
---
+++ teaspoon/src/main/java/com/kh/teaspoon/admin/model/service/AdminService.java
@@ -0,0 +1,5 @@
+package com.kh.teaspoon.admin.model.service;
+
+public interface AdminService {
+
+}
