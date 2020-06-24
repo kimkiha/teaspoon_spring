3396cc9eece15973fb164113aaa241ea5ba56f03
ParkDaeGwang <iieorhkd@naver.com>
2020-06-24 16:23:21 +0900

    어드민작업중

--- teaspoon/src/main/java/com/kh/teaspoon/admin/controller/AdminCotroller.java
+++ teaspoon/src/main/java/com/kh/teaspoon/admin/controller/AdminCotroller.java
@@ -1,8 +1,13 @@
 package com.kh.teaspoon.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminCotroller {
 
+	@RequestMapping("level.me")
+	public String enrollForm() {
+		return "admin/admin_level";
+	}
 }
--- teaspoon/src/main/java/com/kh/teaspoon/admin/model/service/AdminService.java
+++ teaspoon/src/main/java/com/kh/teaspoon/admin/model/service/AdminService.java
@@ -1,5 +1,5 @@
 package com.kh.teaspoon.admin.model.service;
 
 public interface AdminService {
-
+	int memberList();
 }
