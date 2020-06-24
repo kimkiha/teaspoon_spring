0ab35607a0143e93fe4f7ff27143b0cd5ea49a7a
KIHA <flklc901@gmail.com>
2020-06-24 23:31:04 +0900

    수정_기하
    수정_기하

--- teaspoon/src/main/java/com/kh/teaspoon/board/model/service/BoardServiceImpl.java
+++ teaspoon/src/main/java/com/kh/teaspoon/board/model/service/BoardServiceImpl.java
@@ -1,5 +1,17 @@
 package com.kh.teaspoon.board.model.service;
 
-public class BoardServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.kh.teaspoon.board.model.dao.BoardDao;
+
+@Service("bService")
+public class BoardServiceImpl implements BoardService{
+
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	@Autowired
+	private BoardDao bDao;
 }
