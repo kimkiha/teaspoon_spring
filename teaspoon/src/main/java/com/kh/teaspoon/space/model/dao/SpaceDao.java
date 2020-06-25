17cd22648db48c99838a0e3ab4adfc4462ebff73
ssukpuk <63151523+ssukpuk@users.noreply.github.com>
2020-06-25 15:37:12 +0900

    Revert "space.controller"
    This reverts commit 89fa739fb694bef8073a4ca6da1ec3de68241bb0.

--- teaspoon/src/main/java/com/kh/teaspoon/space/model/dao/SpaceDao.java
+++ teaspoon/src/main/java/com/kh/teaspoon/space/model/dao/SpaceDao.java
@@ -1,18 +1,5 @@
 package com.kh.teaspoon.space.model.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.stereotype.Repository;
-
-import com.kh.teaspoon.space.model.vo.Space;
-
-@Repository("scDao")
 public class SpaceDao {
 
-	//공간 대여 등록
-	public int insertSpace(SqlSessionTemplate sqlSession, Space s) {
-		
-		return sqlSession.insert("spaceMapper.insertSpace", s);
-	
-	}
-	
 }
