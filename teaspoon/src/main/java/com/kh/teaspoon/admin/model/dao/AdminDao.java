49a663f05df2581d182e9f2e2ed60202193fcb3f
ParkDaeGwang <iieorhkd@naver.com>
2020-06-25 16:51:20 +0900

    등급추가진행중

--- teaspoon/src/main/java/com/kh/teaspoon/admin/model/dao/AdminDao.java
+++ teaspoon/src/main/java/com/kh/teaspoon/admin/model/dao/AdminDao.java
@@ -2,14 +2,18 @@ package com.kh.teaspoon.admin.model.dao;
 
 import java.util.ArrayList;
 
-import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.teaspoon.admin.model.vo.Grade;
 
 @Repository("adDao")
 public class AdminDao {
-	public ArrayList<Grade> selectGradeList(SqlSession sqlSession){
+	public ArrayList<Grade> selectGradeList(SqlSessionTemplate sqlSession){
 		return (ArrayList)sqlSession.selectList("adminMapper.selectGradeList");
+	}
+
+	public int insertGrade(SqlSessionTemplate sqlSession, Grade grade) {
+		return sqlSession.selectOne("adminMapper.insertGrade",grade);
 	}
 }
